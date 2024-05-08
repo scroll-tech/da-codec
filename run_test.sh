c4d0a7038d49bc07fcf560379d531a7e400d2152
colinlyguo <colinlyguo@scroll.io>
2024-05-08 12:52:33 +0800

    revert

--- run_test.sh
+++ run_test.sh
@@ -2,9 +2,9 @@
 
 # Compile libzstd
 cd libzstd && cargo build --release && cd ..
-sudo cp -f $(pwd)/libzstd/target/release/libscrollzstd.so $(pwd)/
+sudo cp -f $(pwd)/libzstd/target/release/libscroll_zstd.so $(pwd)/
 find $(pwd)/libzstd/target/release | grep libzktrie.so | xargs -I{} cp -f {} $(pwd)/
-sudo cp -f libscrollzstd.so libzktrie.so /usr/local/lib
+sudo cp -f libscroll_zstd.so libzktrie.so /usr/local/lib
 
 # Set the environment variable
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
