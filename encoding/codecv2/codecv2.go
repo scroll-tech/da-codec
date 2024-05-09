f1562af19503e7dfd186cc3dd9eab4184d660049
colinlyguo <colinlyguo@scroll.io>
2024-05-09 21:56:25 +0800

    add -L/lib/ -Wl,-rpath=/lib

--- encoding/codecv2/codecv2.go
+++ encoding/codecv2/codecv2.go
@@ -1,7 +1,7 @@
 package codecv2
 
 /*
-#cgo LDFLAGS: -lm -ldl -lscroll_zstd
+#cgo LDFLAGS: -lm -ldl -lscroll_zstd -L${SRCDIR}/lib/ -Wl,-rpath=${SRCDIR}/lib
 #include <stdint.h>
 char* compress_scroll_batch_bytes(uint8_t* src, uint64_t src_size, uint8_t* output_buf, uint64_t *output_buf_size);
 */
