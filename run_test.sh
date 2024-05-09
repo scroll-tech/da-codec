b138b33798899f86ba1faf247d73a214d156567e
colinlyguo <colinlyguo@scroll.io>
2024-05-10 00:07:32 +0800

    fix

--- run_test.sh
+++ run_test.sh
@@ -4,13 +4,12 @@
 cd libzstd && cargo build --release && cd ..
 sudo cp -f $(pwd)/libzstd/target/release/libscroll_zstd.so $(pwd)/
 find $(pwd)/libzstd/target/release | grep libzktrie.so | xargs -I{} cp -f {} $(pwd)/
-sudo cp -f libscroll_zstd.so libzktrie.so /workspace/lib
 
 # Set the environment variable
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/workspace/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)
 
 # Set CGO_LDFLAGS
-export CGO_LDFLAGS="-L/workspace/lib -lscroll_zstd -lzktrie"
+export CGO_LDFLAGS="-L$(pwd) -lscroll_zstd -lzktrie"
 
 # Run module tests
 go test -v -race -gcflags="-l" -ldflags="-s=false" -coverprofile=coverage.txt -covermode=atomic ./...
