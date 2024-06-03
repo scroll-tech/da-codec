a86900ebd8b71031f47f37c153c259691a9aea38
noelwei <fan@scroll.io>
2024-06-03 15:39:04 +0900

    replace dynamic to static lib

--- run_test.sh
+++ run_test.sh
@@ -2,12 +2,12 @@
 
 # Compile libzstd
 cd libzstd && make libzstd && cd ..
-sudo mkdir -p /scroll/lib/
-sudo cp -f $(pwd)/libzstd/target/release/libscroll_zstd.so /scroll/lib/
+#sudo mkdir -p /scroll/lib/
+#sudo cp -f $(pwd)/libzstd/target/release/libscroll_zstd.so /scroll/lib/
 
 # Set the environment variable
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/scroll/lib/
-export CGO_LDFLAGS="-L/scroll/lib/ -Wl,-rpath,/scroll/lib/"
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/scroll/lib/
+# export CGO_LDFLAGS="-L/scroll/lib/ -Wl,-rpath,/scroll/lib/"
 
 # Run unit tests
 go test -v -race -gcflags="-l" -ldflags="-s=false" -coverprofile=coverage.txt -covermode=atomic ./...
