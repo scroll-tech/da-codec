536d30268579784b4876fb0582d634ebb0a6baa3
colinlyguo <colinlyguo@scroll.io>
2024-07-10 22:05:29 +0800

    fix unit tests

--- encoding/codecv2/codecv2.go
+++ encoding/codecv2/codecv2.go
@@ -185,7 +185,7 @@ func ConstructBlobPayload(chunks []*encoding.Chunk, useMockTxData bool) (*kzg484
 	}
 
 	// Only apply this check when the uncompressed batch data has exceeded 128 KiB.
-	if len(blobBytes) > 131072 {
+	if !useMockTxData && len(blobBytes) > 131072 {
 		// Check compressed data compatibility.
 		if err = encoding.CheckCompressedDataCompatibility(compressedBlobBytes); err != nil {
 			log.Error("ConstructBlobPayload: compressed data compatibility check failed", "err", err, "uncompressedBlobBytes", hex.EncodeToString(blobBytes), "compressedBlobBytes", hex.EncodeToString(compressedBlobBytes))
