643bb086572d7603a99f8afcb5e6c2f4201cd18c
colinlyguo <colinlyguo@scroll.io>
2024-07-22 22:41:00 +0800

    increase compression level

--- libzstd/encoder/src/lib.rs
+++ libzstd/encoder/src/lib.rs
@@ -4,25 +4,25 @@ use zstd::zstd_safe::{CParameter, ParamSwitch};
 // re-export zstd
 pub use zstd;
 
-// we use offset window no more than = 1000
+// we use offset window no more than = 25
 // TODO: use for multi-block zstd.
 #[allow(dead_code)]
-pub const CL_WINDOW_LIMIT: usize = 1000;
+pub const CL_WINDOW_LIMIT: usize = 25;
 
 /// Maximum number of blocks that we can expect in the encoded data.
 pub const N_MAX_BLOCKS: u64 = 10;
 
 /// Zstd encoder configuration
 pub fn init_zstd_encoder() -> Encoder<'static, Vec<u8>> {
-    let mut encoder = Encoder::new(Vec::new(), 0).expect("infallible");
+    let mut encoder = Encoder::new(Vec::new(), 22).expect("infallible");
 
     // disable compression of literals, i.e. literals will be raw bytes.
     encoder
         .set_parameter(CParameter::LiteralCompressionMode(ParamSwitch::Disable))
         .expect("infallible");
-    // with a hack in zstd we can set window log <= 1000 with single segment kept
+    // with a hack in zstd we can set window log <= 25 with single segment kept
     encoder
-        .set_parameter(CParameter::WindowLog(1000))
+        .set_parameter(CParameter::WindowLog(25))
         .expect("infallible");
     // do not include the checksum at the end of the encoded data.
     encoder.include_checksum(false).expect("infallible");
