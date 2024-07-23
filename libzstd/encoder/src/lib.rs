031e94963da42ba34a76b8458a522c5b839212d0
colinlyguo <colinlyguo@scroll.io>
2024-07-23 15:17:29 +0800

    add back CL_WINDOW_LIMIT

--- libzstd/encoder/src/lib.rs
+++ libzstd/encoder/src/lib.rs
@@ -4,6 +4,11 @@ use zstd::zstd_safe::{CParameter, ParamSwitch};
 // re-export zstd
 pub use zstd;
 
+// we use offset window no more than = 17
+// TODO: use for multi-block zstd.
+#[allow(dead_code)]
+pub const CL_WINDOW_LIMIT: usize = 17;
+
 /// zstd block size target.
 pub const N_BLOCK_SIZE_TARGET: u32 = 124 * 1024;
 
@@ -18,9 +23,9 @@ pub fn init_zstd_encoder(target_block_size: u32) -> Encoder<'static, Vec<u8>> {
     encoder
         .set_parameter(CParameter::LiteralCompressionMode(ParamSwitch::Disable))
         .expect("infallible");
-    // with a hack in zstd we can set window log <= 17 with single segment kept
+    // with a hack in zstd we can set window log <= CL_WINDOW_LIMIT with single segment kept
     encoder
-        .set_parameter(CParameter::WindowLog(17))
+        .set_parameter(CParameter::WindowLog(CL_WINDOW_LIMIT))
         .expect("infallible");
     // set target block size to fit within a single block.
     encoder
