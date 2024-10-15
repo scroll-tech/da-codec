5c3c05577aba31501e6146f4633144005951c34d
colinlyguo <colinlyguo@scroll.io>
2024-10-16 00:04:17 +0800

    add back test block encode and test chunk encode unit tests

---
+++ encoding/codecv3_test.go
@@ -0,0 +1,144 @@
+package encoding
+
+import (
+	"encoding/hex"
+	"testing"
+
+	"github.com/scroll-tech/go-ethereum/core/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCodecV3BlockEncode(t *testing.T) {
+	codecv3, err := CodecFromVersion(CodecV3)
+	assert.NoError(t, err)
+
+	block := &daBlockV0{}
+	encoded := hex.EncodeToString(block.Encode())
+	assert.Equal(t, "000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", encoded)
+
+	block2 := readBlockFromJSON(t, "testdata/blockTrace_02.json")
+	daBlock, err := codecv3.NewDABlock(block2, 0)
+	assert.NoError(t, err)
+	encoded = hex.EncodeToString(daBlock.Encode())
+	assert.Equal(t, "00000000000000020000000063807b2a0000000000000000000000000000000000000000000000000000000000001de9000355418d1e818400020000", encoded)
+
+	block3 := readBlockFromJSON(t, "testdata/blockTrace_03.json")
+	daBlock, err = codecv3.NewDABlock(block3, 0)
+	assert.NoError(t, err)
+	encoded = hex.EncodeToString(daBlock.Encode())
+	assert.Equal(t, "00000000000000030000000063807b2d0000000000000000000000000000000000000000000000000000000000001a2c0003546c3cbb39e500010000", encoded)
+
+	block4 := readBlockFromJSON(t, "testdata/blockTrace_04.json")
+	daBlock, err = codecv3.NewDABlock(block4, 0)
+	assert.NoError(t, err)
+	encoded = hex.EncodeToString(daBlock.Encode())
+	assert.Equal(t, "000000000000000d00000000646b6e13000000000000000000000000000000000000000000000000000000000000000000000000007a1200000c000b", encoded)
+
+	block5 := readBlockFromJSON(t, "testdata/blockTrace_05.json")
+	daBlock, err = codecv3.NewDABlock(block5, 0)
+	assert.NoError(t, err)
+	encoded = hex.EncodeToString(daBlock.Encode())
+	assert.Equal(t, "000000000000001100000000646b6ed0000000000000000000000000000000000000000000000000000000000000000000000000007a1200002a002a", encoded)
+
+	block6 := readBlockFromJSON(t, "testdata/blockTrace_06.json")
+	daBlock, err = codecv3.NewDABlock(block6, 0)
+	assert.NoError(t, err)
+	encoded = hex.EncodeToString(daBlock.Encode())
+	assert.Equal(t, "000000000000001100000000646b6ed0000000000000000000000000000000000000000000000000000000000000000000000000007a1200000a000a", encoded)
+
+	block7 := readBlockFromJSON(t, "testdata/blockTrace_07.json")
+	daBlock, err = codecv3.NewDABlock(block7, 0)
+	assert.NoError(t, err)
+	encoded = hex.EncodeToString(daBlock.Encode())
+	assert.Equal(t, "000000000000001100000000646b6ed0000000000000000000000000000000000000000000000000000000000000000000000000007a120001010101", encoded)
+
+	codecV0, err := CodecFromVersion(CodecV0)
+	assert.NoError(t, err)
+
+	// sanity check: v0 and v3 block encodings are identical
+	for _, trace := range []*Block{block2, block3, block4, block5, block6, block7} {
+		blockv0, err := codecV0.NewDABlock(trace, 0)
+		assert.NoError(t, err)
+		encodedv0 := hex.EncodeToString(blockv0.Encode())
+
+		blockv3, err := codecv3.NewDABlock(trace, 0)
+		assert.NoError(t, err)
+		encodedv3 := hex.EncodeToString(blockv3.Encode())
+
+		assert.Equal(t, encodedv0, encodedv3)
+	}
+}
+func TestCodecV3ChunkEncode(t *testing.T) {
+	codecv3, err := CodecFromVersion(CodecV3)
+	assert.NoError(t, err)
+
+	// chunk with a single empty block
+	daBlock := &daBlockV0{}
+	daChunkV1 := &daChunkV1{blocks: []DABlock{daBlock}, transactions: [][]*types.TransactionData{nil}}
+
+	encodedBytes, err := daChunkV1.Encode()
+	assert.NoError(t, err)
+	encoded := hex.EncodeToString(encodedBytes)
+	assert.Equal(t, "01000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", encoded)
+
+	// transactions are not part of the encoding
+	daChunkV1.transactions[0] = append(daChunkV1.transactions[0], &types.TransactionData{Type: types.L1MessageTxType}, &types.TransactionData{Type: types.DynamicFeeTxType})
+	encodedBytes, err = daChunkV1.Encode()
+	assert.NoError(t, err)
+	encoded = hex.EncodeToString(encodedBytes)
+	assert.Equal(t, "01000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", encoded)
+
+	block := readBlockFromJSON(t, "testdata/blockTrace_02.json")
+	originalChunk := &Chunk{Blocks: []*Block{block}}
+	daChunk, err := codecv3.NewDAChunk(originalChunk, 0)
+	assert.NoError(t, err)
+	encodedBytes, err = daChunk.Encode()
+	assert.NoError(t, err)
+	encoded = hex.EncodeToString(encodedBytes)
+	assert.Equal(t, "0100000000000000020000000063807b2a0000000000000000000000000000000000000000000000000000000000001de9000355418d1e818400020000", encoded)
+
+	block = readBlockFromJSON(t, "testdata/blockTrace_03.json")
+	originalChunk = &Chunk{Blocks: []*Block{block}}
+	daChunk, err = codecv3.NewDAChunk(originalChunk, 0)
+	assert.NoError(t, err)
+	encodedBytes, err = daChunk.Encode()
+	assert.NoError(t, err)
+	encoded = hex.EncodeToString(encodedBytes)
+	assert.Equal(t, "0100000000000000030000000063807b2d0000000000000000000000000000000000000000000000000000000000001a2c0003546c3cbb39e500010000", encoded)
+
+	block = readBlockFromJSON(t, "testdata/blockTrace_04.json")
+	originalChunk = &Chunk{Blocks: []*Block{block}}
+	daChunk, err = codecv3.NewDAChunk(originalChunk, 0)
+	assert.NoError(t, err)
+	encodedBytes, err = daChunk.Encode()
+	assert.NoError(t, err)
+	encoded = hex.EncodeToString(encodedBytes)
+	assert.Equal(t, "01000000000000000d00000000646b6e13000000000000000000000000000000000000000000000000000000000000000000000000007a1200000c000b", encoded)
+
+	block = readBlockFromJSON(t, "testdata/blockTrace_05.json")
+	originalChunk = &Chunk{Blocks: []*Block{block}}
+	daChunk, err = codecv3.NewDAChunk(originalChunk, 0)
+	assert.NoError(t, err)
+	encodedBytes, err = daChunk.Encode()
+	assert.NoError(t, err)
+	encoded = hex.EncodeToString(encodedBytes)
+	assert.Equal(t, "01000000000000001100000000646b6ed0000000000000000000000000000000000000000000000000000000000000000000000000007a1200002a002a", encoded)
+
+	block = readBlockFromJSON(t, "testdata/blockTrace_06.json")
+	originalChunk = &Chunk{Blocks: []*Block{block}}
+	daChunk, err = codecv3.NewDAChunk(originalChunk, 0)
+	assert.NoError(t, err)
+	encodedBytes, err = daChunk.Encode()
+	assert.NoError(t, err)
+	encoded = hex.EncodeToString(encodedBytes)
+	assert.Equal(t, "01000000000000001100000000646b6ed0000000000000000000000000000000000000000000000000000000000000000000000000007a1200000a000a", encoded)
+
+	block = readBlockFromJSON(t, "testdata/blockTrace_07.json")
+	originalChunk = &Chunk{Blocks: []*Block{block}}
+	daChunk, err = codecv3.NewDAChunk(originalChunk, 0)
+	assert.NoError(t, err)
+	encodedBytes, err = daChunk.Encode()
+	assert.NoError(t, err)
+	encoded = hex.EncodeToString(encodedBytes)
+	assert.Equal(t, "01000000000000001100000000646b6ed0000000000000000000000000000000000000000000000000000000000000000000000000007a120001010101", encoded)
+}
---
+++ encoding/codecv4_test.go
@@ -0,0 +1,145 @@
+package encoding
+
+import (
+	"encoding/hex"
+	"testing"
+
+	"github.com/scroll-tech/go-ethereum/core/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCodecV4BlockEncode(t *testing.T) {
+	codecv4, err := CodecFromVersion(CodecV4)
+	assert.NoError(t, err)
+
+	block := &daBlockV0{}
+	encoded := hex.EncodeToString(block.Encode())
+	assert.Equal(t, "000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", encoded)
+
+	block2 := readBlockFromJSON(t, "testdata/blockTrace_02.json")
+	daBlock, err := codecv4.NewDABlock(block2, 0)
+	assert.NoError(t, err)
+	encoded = hex.EncodeToString(daBlock.Encode())
+	assert.Equal(t, "00000000000000020000000063807b2a0000000000000000000000000000000000000000000000000000000000001de9000355418d1e818400020000", encoded)
+
+	block3 := readBlockFromJSON(t, "testdata/blockTrace_03.json")
+	daBlock, err = codecv4.NewDABlock(block3, 0)
+	assert.NoError(t, err)
+	encoded = hex.EncodeToString(daBlock.Encode())
+	assert.Equal(t, "00000000000000030000000063807b2d0000000000000000000000000000000000000000000000000000000000001a2c0003546c3cbb39e500010000", encoded)
+
+	block4 := readBlockFromJSON(t, "testdata/blockTrace_04.json")
+	daBlock, err = codecv4.NewDABlock(block4, 0)
+	assert.NoError(t, err)
+	encoded = hex.EncodeToString(daBlock.Encode())
+	assert.Equal(t, "000000000000000d00000000646b6e13000000000000000000000000000000000000000000000000000000000000000000000000007a1200000c000b", encoded)
+
+	block5 := readBlockFromJSON(t, "testdata/blockTrace_05.json")
+	daBlock, err = codecv4.NewDABlock(block5, 0)
+	assert.NoError(t, err)
+	encoded = hex.EncodeToString(daBlock.Encode())
+	assert.Equal(t, "000000000000001100000000646b6ed0000000000000000000000000000000000000000000000000000000000000000000000000007a1200002a002a", encoded)
+
+	block6 := readBlockFromJSON(t, "testdata/blockTrace_06.json")
+	daBlock, err = codecv4.NewDABlock(block6, 0)
+	assert.NoError(t, err)
+	encoded = hex.EncodeToString(daBlock.Encode())
+	assert.Equal(t, "000000000000001100000000646b6ed0000000000000000000000000000000000000000000000000000000000000000000000000007a1200000a000a", encoded)
+
+	block7 := readBlockFromJSON(t, "testdata/blockTrace_07.json")
+	daBlock, err = codecv4.NewDABlock(block7, 0)
+	assert.NoError(t, err)
+	encoded = hex.EncodeToString(daBlock.Encode())
+	assert.Equal(t, "000000000000001100000000646b6ed0000000000000000000000000000000000000000000000000000000000000000000000000007a120001010101", encoded)
+
+	codecV0, err := CodecFromVersion(CodecV0)
+	assert.NoError(t, err)
+
+	// sanity check: v0 and v4 block encodings are identical
+	for _, trace := range []*Block{block2, block3, block4, block5, block6, block7} {
+		blockv0, err := codecV0.NewDABlock(trace, 0)
+		assert.NoError(t, err)
+		encodedv0 := hex.EncodeToString(blockv0.Encode())
+
+		blockv4, err := codecv4.NewDABlock(trace, 0)
+		assert.NoError(t, err)
+		encodedv4 := hex.EncodeToString(blockv4.Encode())
+
+		assert.Equal(t, encodedv0, encodedv4)
+	}
+}
+
+func TestCodecV4ChunkEncode(t *testing.T) {
+	codecv4, err := CodecFromVersion(CodecV4)
+	assert.NoError(t, err)
+
+	// chunk with a single empty block
+	daBlock := &daBlockV0{}
+	daChunkV1 := &daChunkV1{blocks: []DABlock{daBlock}, transactions: [][]*types.TransactionData{nil}}
+
+	encodedBytes, err := daChunkV1.Encode()
+	assert.NoError(t, err)
+	encoded := hex.EncodeToString(encodedBytes)
+	assert.Equal(t, "01000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", encoded)
+
+	// transactions are not part of the encoding
+	daChunkV1.transactions[0] = append(daChunkV1.transactions[0], &types.TransactionData{Type: types.L1MessageTxType}, &types.TransactionData{Type: types.DynamicFeeTxType})
+	encodedBytes, err = daChunkV1.Encode()
+	assert.NoError(t, err)
+	encoded = hex.EncodeToString(encodedBytes)
+	assert.Equal(t, "01000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", encoded)
+
+	block := readBlockFromJSON(t, "testdata/blockTrace_02.json")
+	originalChunk := &Chunk{Blocks: []*Block{block}}
+	daChunk, err := codecv4.NewDAChunk(originalChunk, 0)
+	assert.NoError(t, err)
+	encodedBytes, err = daChunk.Encode()
+	assert.NoError(t, err)
+	encoded = hex.EncodeToString(encodedBytes)
+	assert.Equal(t, "0100000000000000020000000063807b2a0000000000000000000000000000000000000000000000000000000000001de9000355418d1e818400020000", encoded)
+
+	block = readBlockFromJSON(t, "testdata/blockTrace_03.json")
+	originalChunk = &Chunk{Blocks: []*Block{block}}
+	daChunk, err = codecv4.NewDAChunk(originalChunk, 0)
+	assert.NoError(t, err)
+	encodedBytes, err = daChunk.Encode()
+	assert.NoError(t, err)
+	encoded = hex.EncodeToString(encodedBytes)
+	assert.Equal(t, "0100000000000000030000000063807b2d0000000000000000000000000000000000000000000000000000000000001a2c0003546c3cbb39e500010000", encoded)
+
+	block = readBlockFromJSON(t, "testdata/blockTrace_04.json")
+	originalChunk = &Chunk{Blocks: []*Block{block}}
+	daChunk, err = codecv4.NewDAChunk(originalChunk, 0)
+	assert.NoError(t, err)
+	encodedBytes, err = daChunk.Encode()
+	assert.NoError(t, err)
+	encoded = hex.EncodeToString(encodedBytes)
+	assert.Equal(t, "01000000000000000d00000000646b6e13000000000000000000000000000000000000000000000000000000000000000000000000007a1200000c000b", encoded)
+
+	block = readBlockFromJSON(t, "testdata/blockTrace_05.json")
+	originalChunk = &Chunk{Blocks: []*Block{block}}
+	daChunk, err = codecv4.NewDAChunk(originalChunk, 0)
+	assert.NoError(t, err)
+	encodedBytes, err = daChunk.Encode()
+	assert.NoError(t, err)
+	encoded = hex.EncodeToString(encodedBytes)
+	assert.Equal(t, "01000000000000001100000000646b6ed0000000000000000000000000000000000000000000000000000000000000000000000000007a1200002a002a", encoded)
+
+	block = readBlockFromJSON(t, "testdata/blockTrace_06.json")
+	originalChunk = &Chunk{Blocks: []*Block{block}}
+	daChunk, err = codecv4.NewDAChunk(originalChunk, 0)
+	assert.NoError(t, err)
+	encodedBytes, err = daChunk.Encode()
+	assert.NoError(t, err)
+	encoded = hex.EncodeToString(encodedBytes)
+	assert.Equal(t, "01000000000000001100000000646b6ed0000000000000000000000000000000000000000000000000000000000000000000000000007a1200000a000a", encoded)
+
+	block = readBlockFromJSON(t, "testdata/blockTrace_07.json")
+	originalChunk = &Chunk{Blocks: []*Block{block}}
+	daChunk, err = codecv4.NewDAChunk(originalChunk, 0)
+	assert.NoError(t, err)
+	encodedBytes, err = daChunk.Encode()
+	assert.NoError(t, err)
+	encoded = hex.EncodeToString(encodedBytes)
+	assert.Equal(t, "01000000000000001100000000646b6ed0000000000000000000000000000000000000000000000000000000000000000000000000007a120001010101", encoded)
+}
