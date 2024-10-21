8c5d2f0cd707153151a5154fef702204f6ca40b3
colin <102356659+colinlyguo@users.noreply.github.com>
2024-10-21 16:12:45 +0800

    refactor: make IsL1MessageSkipped public (#30)

--- encoding/bitmap_test.go
+++ encoding/bitmap_test.go
@@ -2,7 +2,6 @@ package encoding
 
 import (
 	"encoding/hex"
-	"math/big"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,25 +15,16 @@ func TestDecodeBitmap(t *testing.T) {
 	decodedBitmap, err := DecodeBitmap(skippedL1MessageBitmap, 42)
 	assert.NoError(t, err)
 
-	isL1MessageSkipped := func(skippedBitmap []*big.Int, index uint64) bool {
-		if index >= uint64(len(skippedBitmap))*256 {
-			return false
-		}
-		quo := index / 256
-		rem := index % 256
-		return skippedBitmap[quo].Bit(int(rem)) == 1
-	}
-
-	assert.True(t, isL1MessageSkipped(decodedBitmap, 0))
-	assert.True(t, isL1MessageSkipped(decodedBitmap, 9))
-	assert.False(t, isL1MessageSkipped(decodedBitmap, 10))
-	assert.True(t, isL1MessageSkipped(decodedBitmap, 11))
-	assert.True(t, isL1MessageSkipped(decodedBitmap, 36))
-	assert.False(t, isL1MessageSkipped(decodedBitmap, 37))
-	assert.False(t, isL1MessageSkipped(decodedBitmap, 38))
-	assert.False(t, isL1MessageSkipped(decodedBitmap, 39))
-	assert.False(t, isL1MessageSkipped(decodedBitmap, 40))
-	assert.False(t, isL1MessageSkipped(decodedBitmap, 41))
+	assert.True(t, IsL1MessageSkipped(decodedBitmap, 0))
+	assert.True(t, IsL1MessageSkipped(decodedBitmap, 9))
+	assert.False(t, IsL1MessageSkipped(decodedBitmap, 10))
+	assert.True(t, IsL1MessageSkipped(decodedBitmap, 11))
+	assert.True(t, IsL1MessageSkipped(decodedBitmap, 36))
+	assert.False(t, IsL1MessageSkipped(decodedBitmap, 37))
+	assert.False(t, IsL1MessageSkipped(decodedBitmap, 38))
+	assert.False(t, IsL1MessageSkipped(decodedBitmap, 39))
+	assert.False(t, IsL1MessageSkipped(decodedBitmap, 40))
+	assert.False(t, IsL1MessageSkipped(decodedBitmap, 41))
 
 	_, err = DecodeBitmap([]byte{0x00}, 8)
 	assert.Error(t, err)
